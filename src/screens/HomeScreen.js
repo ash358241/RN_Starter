bc487d5204550dd3eac6ace22d8b2f8e80d5e616
Ashraful Islam <ashrafunit7@gmail.com>
2021-12-13 21:10:50 +0600

    TouchableOpacity(main btn kind) in action

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
+import { TouchableOpacity } from "react-native-web";
 
 const HomeScreen = () => {
-  return(
+  return (
     <View>
       <Text style={styles.text}>Hi React Native!!!</Text>
-      <Button 
+      {/* <Button 
       onPress={() => console.log('Button pressed')}
-      title="Go to component demo" />
+      title="Go to component demo" /> */}
+      <TouchableOpacity
+        onPress={() => console.log('List pressed')}
+      >
+        <Text>Go to list demo</Text>
+      </TouchableOpacity>
     </View>
   )
 };
