38f53348d27a81e86bb528b055612ff8a981bfe3
Ashraful Islam <ashrafunit7@gmail.com>
2021-12-22 02:22:38 +0600

    passing props to child

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
 const SquareScreen = () => {
+    const [red, setRed] = useState(0);
+    const [green, setGreen] = useState(0);
+    const [blue, setBlue] = useState(0);
+
+    const COLOR_INCREMENT = 15;
+
+    console.log(red);
     return (
         <View>
-            <ColorCounter color="red"></ColorCounter>
-            <ColorCounter color="blue"></ColorCounter>
-            <ColorCounter color="green"></ColorCounter>
+            <ColorCounter onIncrease={() => setRed(red + COLOR_INCREMENT)} onDecrease={() => setRed(red - COLOR_INCREMENT)} color="red"></ColorCounter>
+            <ColorCounter onIncrease={() => setGreen(green + COLOR_INCREMENT)} onDecrease={() => setGreen(green - COLOR_INCREMENT)} color="green"></ColorCounter>
+            <ColorCounter onIncrease={() => setBlue(blue + COLOR_INCREMENT)} onDecrease={() => setBlue(blue - COLOR_INCREMENT)} color="blue"></ColorCounter>
+
+            <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})` }}></View>
         </View>
     );
 };
