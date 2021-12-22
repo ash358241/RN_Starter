8ebe997f7bf8d96299dac1d2220e840d0944930a
Ashraful Islam <ashrafunit7@gmail.com>
2021-12-23 01:49:36 +0600

    playing with textinput stuff

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -3,10 +3,15 @@ import { Text, TextInput, View, StyleSheet } from 'react-native';
 
 const TextScreen = () => {
     const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
     return (
         <View>
             <TextInput style={styles.input} autoCapitalize='none' autoCorrect={false} value={name} onChangeText={newText => setName(newText)} />
+            <TextInput style={styles.input} value={password} onChangeText={newValue => setPassword(newValue)} />
             <Text>{name}</Text>
+            {
+                password.length < 5 ? <Text>Password must be 5 characters</Text> : null
+            }
         </View>
     );
 };
