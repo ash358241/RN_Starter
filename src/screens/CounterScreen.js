d3d0d47b4474f181500223daad7c88120f71ad91
Ashraful Islam <ashrafunit7@gmail.com>
2021-12-22 23:29:01 +0600

    reducer implemented in counter component

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,15 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 
+const INCREMENT = 1;
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'increment':
+            return {...state, counter: state.counter + action.payload};
+        case 'decrement':
+            return {...state, counter: state.counter - action.payload};
+        default:
+            return state;
+    }
+}
+
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    // const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, {counter: 0})
+    const {counter} = state;
     return (
         <View>
             <Button title="increment" onPress={() => {
-                setCounter(counter + 1);
+                // setCounter(counter + 1);
+                dispatch({type: 'increment', payload: INCREMENT})
             }} />
             <Button title="decrement" onPress={() => {
-                setCounter(counter - 1);
+                // setCounter(counter - 1);
+                dispatch({type: 'decrement', payload: INCREMENT})
             }} />
             <Text>Counter Count: {counter}</Text>
         </View>
